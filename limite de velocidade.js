00ac5d6a494df0e7b7d8a79d583bd3016b875ec6
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-30 10:39:17 -0300

    Add files via upload

---
+++ limite de velocidade.js
@@ -0,0 +1,9 @@
+let velocidade = 100;
+
+console.log("A velocidade é de " + velocidade + " Km/h\n");
+
+if(velocidade > 80){
+    console.log("Esta acima da velocidade permitida.");
+} else{
+    console.log("Esta dentro da velocidade permitida");
+}
