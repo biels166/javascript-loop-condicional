df29a514e2e281f4aacf6e71e14375f8a30cf8ab
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-30 04:40:46 -0300

    Add files via upload

---
+++ math.pow().js
@@ -0,0 +1,9 @@
+console.log("Testando a função 'Math( )'\n");
+
+console.log("2 ao quadrado é " + Math.pow(2,2));
+console.log("3 ao quadrado é " + Math.pow(3,2));
+console.log("\nTestanto a função 'Math.pow' com loop for:\n")
+
+for(let i = 1; i <= 20; i+=1){
+    console.log(i + " ao quadrado é " + Math.pow(i,2));
+}
