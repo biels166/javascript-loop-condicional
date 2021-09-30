385c0785646b343a3e5bae3c3f7d8a15b6dc8018
Gabriel Santana <gabriel_santana17@hotmail.com>
2021-09-30 04:09:52 -0300

    Add files via upload

---
+++ declaracao de variaveis.js
@@ -0,0 +1,13 @@
+let nome = "Gabriel";
+let idade= 26;
+let verifica;
+
+console.log("Meu nome é " + nome);
+console.log(typeof nome);
+
+console.log("\nEu tenho " + idade + " anos");
+console.log(typeof idade);
+
+verifica = ((2 > 1) || (10 < 100));
+console.log(`\n${verifica}`);
+console.log(typeof verifica);
